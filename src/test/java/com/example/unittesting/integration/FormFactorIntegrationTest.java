62eb374c827b872153bfb725cd6ebb951d248742
razolly <razli1993@hotmail.com>
2022-09-24 15:33:56 +0800

    Test json response using JSONAssert

--- src/test/java/com/example/unittesting/integration/FormFactorIntegrationTest.java
+++ src/test/java/com/example/unittesting/integration/FormFactorIntegrationTest.java
@@ -1,9 +1,12 @@
 package com.example.unittesting.integration;
 
+import org.json.JSONException;
 import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
 
 // Loads the whole application context, by looking for a class with @SpringBootConfiguration
 // Usually finds the @SpringBootApplication class (with main method)
@@ -11,10 +14,11 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 public class FormFactorIntegrationTest {
 
     @Autowired
-    private TestRestTemplate testRestTemplate;
+    private TestRestTemplate restTemplate;
 
     @Test
-    public void contextLoads() {
-
+    public void contextLoads() throws JSONException {
+        ResponseEntity<String> response = restTemplate.getForEntity("/form-factor-from-service-processed", String.class);
+        JSONAssert.assertEquals("[{id:100},{id:101},{id:102}]", response.getBody(), false);
     }
 }
